fe96e6d27376bcd1ac85bc795aec242f315dba5c
Kateryna <kati.skrinnikova@gmail.com>
2024-08-03 13:55:00 +0300

    component BackgroundColor, Layout, TrackerPage template

---
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -0,0 +1,11 @@
+import BackgroundColor from 'components/BackgroundColor/BackgraundColor';
+import Layout from 'components/Layout/Layout';
+
+export default function TrackerPage() {
+  return (
+    <Layout>
+      <BackgroundColor color="green">Hello, world</BackgroundColor>
+      <BackgroundColor color="grey">Hello, world</BackgroundColor>
+    </Layout>
+  );
+}
