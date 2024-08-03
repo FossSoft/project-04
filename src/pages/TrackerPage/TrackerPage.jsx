d36987a475c5c73c9fdca8d0313b5297eb8069cf
Kateryna <kati.skrinnikova@gmail.com>
2024-08-03 17:33:08 +0300

    added userbar, userpanel

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,11 +1,17 @@
 import BackgroundColor from 'components/BackgroundColor/BackgraundColor';
 import Layout from 'components/Layout/Layout';
+import UserPanel from 'components/UserPanel/UserPanel';
+import css from './TrackerPage.module.css';
 
 export default function TrackerPage() {
   return (
     <Layout>
+      <BackgroundColor color="grey">
+        <div className={css.padding}>
+          <UserPanel />
+        </div>
+      </BackgroundColor>
       <BackgroundColor color="green">Hello, world</BackgroundColor>
-      <BackgroundColor color="grey">Hello, world</BackgroundColor>
     </Layout>
   );
 }
