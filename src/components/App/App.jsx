b893c13f2d49c3f14187ac3f4214ccd7395208d2
PandaTrumpet <dsavontrumpet@gmail.com>
2024-08-03 12:14:38 +0300

    create component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,9 @@
 import styles from './App.module.css';
-
+import { Setting } from 'components/Setting/Setting.jsx';
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Setting />
+    </div>
+  );
 };
