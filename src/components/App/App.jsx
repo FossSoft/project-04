e76b42a2c986dffb87ad186b9acd934d9a7c8940
Andromeda-NGC224 <shadim115115@gmail.com>
2024-08-03 18:19:37 +0300

    Done Edite amount water modal COMPONENT

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
 import styles from './App.module.css';
+import { EditWaterModal } from 'components/EditWaterModal/EditWaterModal.jsx';
 
 export const App = () => {
-  return <div></div>;
+  return <div>
+  </div>;
 };
