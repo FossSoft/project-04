1597cc61e9b052069f3f11e2277615f33266fa86
OlenaYurchuk <145892734+OlenaYurchuk@users.noreply.github.com>
2024-08-03 21:28:02 +0300

    added SignInPage

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+    const { isLoggedIn, isRefreshing } = useAuth();
+    const shouldRedirect = !isLoggedIn && !isRefreshing;
+
+    return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+};
+
+export default PrivateRoute;
---
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -0,0 +1,5 @@
+export default function TrackerPage() {
+	return (
+		<div></div>
+	)
+}
