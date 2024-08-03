0746268415483514137dcf8fe50aee45a5ff9cac
fosssoft <fossmail23@gmail.com>
2024-08-03 20:46:42 +0300

    icon

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,5 @@ import styles from './App.module.css';
 import { EditWaterModal } from 'components/EditWaterModal/EditWaterModal.jsx';
 
 export const App = () => {
-  return <div>
-  </div>;
+  return <div></div>;
 };
