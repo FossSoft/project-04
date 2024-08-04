f4f8a02029330c4c72b92fb3671b1e13d65097fe
Anna <kupriienko16@gmail.com>
2024-08-04 19:49:22 +0100

    commit

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -6,12 +6,13 @@ export default function WelcomeSection (){
   return (
     <section className={css.section}>
       <Logo />
-
+      <div className={css.text}>
       <h2  className={css.subtitle}>Record daily water intake and track</h2>
       <h1 className={css.title}>Water consumption tracker</h1>
+        </div>
       <div className={css.link}>
-        <Link className={css.link_signup} >Try tracker</Link>
-         <Link className={css.link_signin} >Sign In</Link>
+        <Link className={css.link_signup} to ='signup'>Try tracker</Link>
+         <Link className={css.link_signin} to ='signin' >Sign In</Link>
         </div>
     </section>
   );
