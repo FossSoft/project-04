da5deb1c2293fdc6bccf6db65b8fbabbb0c9e79d
Vita <v.mars22@ukr.net>
2024-08-07 15:03:35 +0300

    add media styles

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,15 +1,21 @@
 import Logo from 'components/Logo/Logo';
 import SignUpForm from 'components/SignUpForm/SignUpForm.jsx';
 import css from './SignUpPage.module.css';
+import AdvantagesSection from 'components/AdvantagesSection/AdvantagesSection';
 
 const SignUpPage = () => {
   return (
-    <div className={css.container}>
-      <div className={css.box}>
-        <Logo />
+    <div className={css.wrapper}>
+      <div className={css.container}>
+        <div className={css.box}>
+          <Logo />
+        </div>
+        <div>
+          <SignUpForm />
+        </div>
       </div>
-      <div>
-        <SignUpForm />
+      <div className={css.hideOnTablets}>
+        <AdvantagesSection />
       </div>
     </div>
   );
