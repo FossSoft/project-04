2a7c0b01aaff1a87ad2660c67e1954fbf13621ef
Kateryna <kati.skrinnikova@gmail.com>
2024-08-07 14:55:12 +0300

    added functional in modal logoutmodal and corrected useeffect in userBar

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
@@ -10,8 +10,8 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { authReducer } from "./auth/slice";
-import  useReducer from "./user/slice.js";
+import { authReducer } from './auth/slice';
+import useReducer from './user/slice.js';
 import popoverReducer from '../redux/popover/slice';
 import modalReducer from '../redux/modal/slice';
 import waterReducer from './water/slice';
@@ -23,14 +23,14 @@ const authPersistConfig = {
 };
 
 export const store = configureStore({
-    reducer: {
-        auth: persistReducer(authPersistConfig, authReducer),
-        user: useReducer,
-        popover: popoverReducer,
-        modal: modalReducer,
-        water: waterReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    user: useReducer,
+    popover: popoverReducer,
+    modal: modalReducer,
+    water: waterReducer,
+  },
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
