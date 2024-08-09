5dc6ffc79b60614df521bbfad6d279a43da9e0e2
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-08-09 11:19:01 +0300

    add base settings

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,12 +1,33 @@
-
-import AddWaterBtn from "components/AddWaterBtn/AddWaterBtn";
-import css from './WaterMainInfo.module.css';
+import AddWaterBtn from 'components/AddWaterBtn/AddWaterBtn';
+import styles from './WaterMainInfo.module.css';
+import Logo from 'components/Logo/Logo.jsx';
 
 const WaterMainInfo = () => {
   return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <picture>
+        <source
+          srcset="large-image-1x.jpg 1x, large-image-2x.jpg 2x, large-image-3x.jpg 3x"
+          media="(min-width: 1440px)"
+        />
+        <source
+          srcset="medium-image-1x.jpg 1x, medium-image-2x.jpg 2x, medium-image-3x.jpg 3x"
+          media="(min-width: 768px)"
+        />
+        <source
+          srcset="../../image/x1/transparent_bottle_for_water1_mob.png 1x, ../../image/x2/transparent_bottle_for_water1_mob.png 2x, ../../image/x3/transparent_bottle_for_water1_mob.png 3x"
+          media="(min-width: 320px)"
+        />
+        <img src="../../image/x1/transparent_bottle_for_water1_mob.png" alt="bottle" />
+      </picture>
 
-    <AddWaterBtn isPrimary={true} />
-
+      <div className={styles.addWaterBtn}>
+        <AddWaterBtn isPrimary={true} />
+      </div>
+    </div>
   );
 };
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import WelcomeSection from "components/WelcomeSection/WelcomeSection";
 import AdvantagesSection from "components/AdvantagesSection/AdvantagesSection";
 import Layout from "components/Layout/Layout";
 import BackgroundColor from "components/BackgroundColor/BackgraundColor";
-import { EditWaterModal } from "components/EditWaterModal/EditWaterModal.jsx";
+// import { EditWaterModal } from "components/EditWaterModal/EditWaterModal.jsx";
 export default function HomePage () {
   return (
     <Layout>
