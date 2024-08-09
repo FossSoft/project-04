2c473ae0e7001b50610c61b84624869dab41c17d
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-08-09 09:06:31 +0300

    add some changes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,8 @@
 import { Navigate } from 'react-router-dom';
-// import { useAuth } from '../hooks/useAuth';
-import { useSelector } from 'react-redux';
-import { selectAccessToken } from '../redux/auth/selectors.js';
+import { useAuth } from '../hooks/useAuth';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  // const { isLoggedIn, isRefreshing } = useAuth();
-  // const shouldRedirect = !isLoggedIn;
-
-  const accessToken = useSelector(selectAccessToken);
+  const { accessToken} = useAuth();
 
   return accessToken ? Component : <Navigate to={redirectTo} />;
 };
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,16 +4,19 @@ import {
   selectUser,
   selectIsLoggedIn,
   selectIsRefreshing,
+  selectAccessToken,
 } from '../redux/auth/selectors';
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
+  const accessToken = useSelector(selectAccessToken);
 
   return {
     isLoggedIn,
     isRefreshing,
     user,
+    accessToken
   };
 };
