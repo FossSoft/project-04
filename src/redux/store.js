36241c78c1ec6017ec3bdc69db7e0fc217210b6e
PandaTrumpet <dsavontrumpet@gmail.com>
2024-08-10 23:25:44 +0300

    add css to close  icon

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,9 +19,9 @@ import useReducer from './user/slice.js';
 import popoverReducer from '../redux/popover/slice';
 import modalReducer from '../redux/modal/slice';
 import waterReducer from './water/slice';
-import waterReducer from '../redux/water/slice.js';
-import waterMonthReducer from "../redux/water/calendar/slice.js"
-import { setupAxiosInterceptors } from "./auth/operations";
+
+import waterMonthReducer from '../redux/water/calendar/slice.js';
+import { setupAxiosInterceptors } from './auth/operations';
 
 const authPersistConfig = {
   key: 'auth',
@@ -37,7 +37,7 @@ const rootReducer = combineReducers({
   popover: popoverReducer,
   modal: modalReducer,
   water: waterReducer,
-  waterMonth: waterMonthReducer
+  waterMonth: waterMonthReducer,
 });
 
 const rootReducersWithReset = (state, action) => {
