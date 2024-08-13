0aa0e4dd3f0130cd42cb3b9bdeca6758aa02114a
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-08-13 12:48:49 +0300

    add style

--- src/components/MonthInfo/Calendar/Calendar.jsx
+++ src/components/MonthInfo/Calendar/Calendar.jsx
@@ -1,24 +1,24 @@
 import CalendarItem from '../CalendarItem/CalendarItem';
 import css from './Calendar.module.css';
 
-const Calendar = ({ monthItem, onClick }) => {
+const Calendar = ({ monthItem, onClick, currentDate, selectedDate, isActive }) => {
+  console.log('currentDate from Calendar:', currentDate);
+console.log('selectedDate from Calendar:', selectedDate);
   return (
     <ul className={css.gridWrapper}>
       {monthItem.map(({ date, percentage }, index) => {
         const uniqueKey = index;
-
-        // Логирование date и percentage
-        console.log('Calendar Date:', date);
-        console.log('Calendar Percentage:', percentage);
+        const isActiveItem = selectedDate === date;
 
         return (
           <CalendarItem
             key={uniqueKey}
-            day={date} // Передаем дату как строку в формате 'YYYY-MM-DD'
-            percentageConsumed={percentage} // Передаем соответствующий процент для каждого дня
-            onClick={() => {
-              onClick(date); // Передаем строку даты в формате 'YYYY-MM-DD'
-            }}
+            day={date}
+            percentageConsumed={percentage}
+            onClick={onClick}
+            currentDate={currentDate} // Передаем текущую дату
+            selectedDate={selectedDate}
+            isActive={isActiveItem} // Передаем, активен ли элемент
           />
         );
       })}
--- src/components/MonthInfo/CalendarItem/CalendarItem.jsx
+++ src/components/MonthInfo/CalendarItem/CalendarItem.jsx
@@ -9,23 +9,16 @@ const CalendarItem = ({
   selectedDate,
   isActive,
 }) => {
-  // Преобразование строки даты в формат YYYY-MM-DD
   const dayNumber = parseInt(day.split('-')[2], 10);
-
-  // Логирование значения percentageConsumed
-  console.log('percentageConsumed:', percentageConsumed);
-
-  // Капирование процента (максимум 100)
   const cappedPercentage = Math.min(percentageConsumed, 100);
 
   return (
     <div className={css.container} onClick={() => onClick(day)}>
       <div
         className={clsx(css.number, {
-          [css.notComplete]: cappedPercentage > 0 && cappedPercentage < 100,
-          [css.active]: selectedDate === day && isActive,
-          [css.current]: currentDate === day && !isActive,
-        })}
+          [css.bgHighlighted]: cappedPercentage < 100,
+          [css.current]: currentDate === day ,
+            })}
       >
         {dayNumber}
       </div>
